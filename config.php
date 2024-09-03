77e8e8b835ab2e29cb563cefe2f2e82bb0836641
Dinesh Kumar <mapmydestination@gmail.com>
2024-09-03 19:16:12 +0530

    updated

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+define('_AMSCODESECURITY', '16343942');
+define('CURRENCY', '$');
+define('WEB_URL', 'http://localhost/ams/');
+define('ROOT_PATH', 'C:\wamp\www\ams/');
+
+
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'campcodes');
+define('DB_PASSWORD', 'serbermz2020');
+define('DB_DATABASE', 'ams');
+$link = new mysqli(DB_HOSTNAME,DB_USERNAME,DB_PASSWORD,DB_DATABASE);?>
